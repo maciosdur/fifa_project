da07823ba2272d566230b714377ce4eea6998d93
Maciek <maciej.durkalec@gmail.com>
2025-03-23 22:40:03 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# FIFA 22 Data Analysis Project
+
+## How to run the project
+
+1. Clone this repository.
+2. Install the required libraries by running:
+   ```bash
+   pip install -r requirements.txt
+   ```
---
+++ data_analysis_main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np  
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Load the data
+data = pd.read_csv('players_22.csv')
+
+# Display the first 5 rows of the data
+print(data.head())
